1c34414781f77e95d4cc76f8b6e3c825d759395f
Swapnil Negi <yavnikagarg3435@gmail.com>
2020-05-18 12:12:23 +0530

    Integrate OAuth

--- bugfeed/models/users.py
+++ bugfeed/models/users.py
@@ -5,12 +5,14 @@ class Users(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=40)
     display_picture = models.ImageField(upload_to='assignment/media/', null=True)
-    is_master = models.BooleanField(default=False)
+    is_master = models.BooleanField(default=True)
     enrol_number = models.IntegerField(unique=True)
     branch_name = models.CharField(max_length=80)
     current_year = models.PositiveSmallIntegerField()
     degree_name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
+    access_token = models.CharField(max_length=100, default='', blank=True)
+    refresh_token = models.CharField(max_length=100, default='', blank=True)
 
     @property
     def user_description(self):
